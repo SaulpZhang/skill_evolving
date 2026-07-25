910da596ea2c0bb71da0030a9db21127cdf41e84
saulp <saulp_zhang@foxmail.com>
2026-07-25 20:47:56 +0800

    fix: column_stack shape fix for per-type A/d

--- spg_bandit/modules/selector/spg_bandit/__init__.py
+++ spg_bandit/modules/selector/spg_bandit/__init__.py
@@ -326,11 +326,9 @@ class SPGBanditSelector(BaseSelector):
         self._metrics["mirt_ll_history"] = [round(v, 4) for v in ll_history]
 
         # Embedding → (a, d) predictor: infer parameters for unseen tasks
+        dim_of = [task_pool.metadata[tid]["dim"] for tid in self._warmup_task_ids]
         X_seen = np.array([task_pool.get_embedding(tid) for tid in self._warmup_task_ids])
-        y_seen = np.column_stack([
-            self._A_fit[task_pool.metadata[tid]["dim"]]
-            for tid in self._warmup_task_ids
-        ] + [[self._d_fit[task_pool.metadata[tid]["dim"]]] for tid in self._warmup_task_ids])
+        y_seen = np.column_stack([self._A_fit[dim_of], self._d_fit[dim_of].reshape(-1, 1)])
         reg = Ridge(alpha=self._lambda)
         reg.fit(X_seen, y_seen)
         y_pred_train = reg.predict(X_seen)
